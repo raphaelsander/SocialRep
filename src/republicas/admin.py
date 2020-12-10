3081eb1f7065cf668765ebc0bc884fed1eb4bbe2
Raphael Sander <raphael.sander75@gmail.com>
2020-12-09 23:08:55 -0300

    Edição do nome do App Django para "republics".

--- src/republicas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Post
-
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ("nome", "slug", "autor", "criado", "atualizado")
-    prepopulated_fields = {"slug": ("nome",)}
--- src/republicas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RepublicasConfig(AppConfig):
-    name = 'republicas'
