5ee800df6708cc06a5479e5241540ae94a469818
Raphael Sander <raphael.sander75@gmail.com>
2020-12-12 18:35:53 -0300

    Adição de URL static para arquivos estáticos (css, js, bootstrap).

--- src/socialrep/settings.py
+++ src/socialrep/settings.py
@@ -142,6 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "templates/static",
+]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
