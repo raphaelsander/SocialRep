fbc62dfba7b5ab5d0eebf8f061815f5b1d864f49
Raphael Sander <raphael.sander75@gmail.com>
2020-12-19 11:09:51 -0300

    Adição de atributos ao model Rep.

--- src/republics/models.py
+++ src/republics/models.py
@@ -10,6 +10,17 @@ class Rep(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    img = models.ImageField(upload_to='images/', null=True)
+    # Has in a republic
+    has_animal = models.BooleanField(default=False)
+    has_3d_printer = models.BooleanField(default=False)
+    has_garage = models.BooleanField(default=False)
+    has_grill = models.BooleanField(default=False)
+    has_internet = models.BooleanField(default=False)
+    has_maid = models.BooleanField(default=False)
+    has_pool = models.BooleanField(default=False)
+    has_snooker = models.BooleanField(default=False)
+    has_washing_machine = models.BooleanField(default=False)
 
     class Meta:
         ordering = ("-created",)
