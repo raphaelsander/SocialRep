55a78232a11b0228d950b22812851c3837f234a4
Raphael Sander <raphael.sander75@gmail.com>
2021-01-05 13:20:54 -0300

    Adição de cadastro de república.

--- src/republics/views.py
+++ src/republics/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView, ListView
-
+from django.shortcuts import render, redirect
+from .forms import CreateForm
 from .models import Rep
 
 
@@ -9,3 +10,18 @@ class RepListView(ListView):
 
 class RepDetailView(DetailView):
     model = Rep
+
+
+def createrep(request):
+    if request.user.is_authenticated:
+        form = CreateForm(request.POST, request.FILES or None)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('/republics/detail/' + instance.slug)
+        else:
+            form = CreateForm()
+        return render(request, "republics/rep_create.html", {'form': form})
+    else:
+        return redirect('/accounts/login/')
--- src/socialrep/urls.py
+++ src/socialrep/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from republics.views import createrep
 
 
 urlpatterns = [
@@ -24,4 +25,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path("republics/", include("republics.urls")),
     path("", include("pages.urls", namespace="pages")),
+    path("create/", createrep, name="create")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
